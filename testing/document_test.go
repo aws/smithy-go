cb2e1bdb791bbd22404221aaae6745189ca47676
JordonPhillips <phjordon@amazon.com>
2020-07-22 13:29:39 -0700

    Strip insignificant whitespace from url form body
    This updates the url form equal function to strip out insignificant
    whitespace, which is whitespace that is not percent encoded.

--- testing/document_test.go
+++ testing/document_test.go
@@ -45,6 +45,23 @@ func TestAssertURLFormEqual(t *testing.T) {
 			Y:     []byte(`Action=QueryMaps&Version=2020-01-08&MapArg.entry.2.key=bar&MapArg.entry.2.value=Bar&MapArg.entry.1.key=foo&MapArg.entry.1.value=Foo`),
 			Equal: true,
 		},
+		"strips insignificant whitespace": {
+			X: []byte(`Foo=Bar   &Baz=Bar
+					&Bin=Foo`),
+			Y:     []byte(`Foo=Bar&Baz=Bar&Bin=Foo`),
+			Equal: true,
+		},
+		"preserves significant whitespace": {
+			X:     []byte(`Foo=Bar+&Baz=Bar%20&Bin=Foo%0A`),
+			Y:     []byte(`Foo=Bar%20&Baz=Bar+&Bin=Foo%0A`),
+			Equal: true,
+		},
+		"missing significant whitespace not equal": {
+			X: []byte(`Foo=Bar+&Baz=Bar%20&Bin=Foo%0A%09%09%09%09&Spam=Eggs`),
+			Y: []byte(`Foo=Bar &Baz=Bar &Bin=Foo
+				&Spam=Eggs`),
+			Equal: false,
+		},
 		"not equal": {
 			X:     []byte(`Action=QueryMaps&Version=2020-01-08&MapArg.entry.1.key=foo&MapArg.entry.1.value=Foo&MapArg.entry.2.key=bar&MapArg.entry.2.value=Bar`),
 			Y:     []byte(`Action=QueryMaps&Version=2020-01-08&MapArg.entry.1.key=foo&MapArg.entry.1.value=Foo`),
