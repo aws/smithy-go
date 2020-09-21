30eba8a9ff05b5cbefd157d81f9f155816e7bc14
skotambkar <kotambka@amazon.com>
2020-09-21 12:47:05 -0700

    xml util changed to deser error message along with code

--- xml/error_utils_test.go
+++ xml/error_utils_test.go
@@ -12,6 +12,7 @@ func TestGetResponseErrorCode(t *testing.T) {
 		errorResponse          io.Reader
 		noErrorWrappingEnabled bool
 		expectedErrorCode      string
+		expectedErrorMessage   string
 	}{
 		"no error wrapping enabled": {
 			errorResponse: bytes.NewReader([]byte(`<ErrorResponse>
@@ -23,7 +24,8 @@ func TestGetResponseErrorCode(t *testing.T) {
     </Error>
     <RequestId>foo-id</RequestId>
 </ErrorResponse>`)),
-			expectedErrorCode: "InvalidGreeting",
+			expectedErrorCode:    "InvalidGreeting",
+			expectedErrorMessage: "Hi",
 		},
 		"no error wrapping disabled": {
 			errorResponse: bytes.NewReader([]byte(`<ErrorResponse>
@@ -35,17 +37,21 @@ func TestGetResponseErrorCode(t *testing.T) {
 </ErrorResponse>`)),
 			noErrorWrappingEnabled: true,
 			expectedErrorCode:      "InvalidGreeting",
+			expectedErrorMessage:   "Hi",
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			errorcode, err := GetResponseErrorCode(c.errorResponse, c.noErrorWrappingEnabled)
+			ec, err := GetErrorResponseComponents(c.errorResponse, c.noErrorWrappingEnabled)
 			if err != nil {
 				t.Fatalf("expected no error, got %v", err)
 			}
 
-			if e, a := c.expectedErrorCode, errorcode; !strings.EqualFold(e, a) {
+			if e, a := c.expectedErrorCode, ec.Code; !strings.EqualFold(e, a) {
+				t.Fatalf("expected %v, got %v", e, a)
+			}
+			if e, a := c.expectedErrorMessage, ec.Message; !strings.EqualFold(e, a) {
 				t.Fatalf("expected %v, got %v", e, a)
 			}
 		})
