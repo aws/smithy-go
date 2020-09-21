f33ec667df15d3168355b83338f1ba43b85f6249
skotambkar <kotambka@amazon.com>
2020-09-21 12:48:55 -0700

    codegen changes to return metadata when deserializing top level error document shapes

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/integration/HttpProtocolGeneratorUtils.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/integration/HttpProtocolGeneratorUtils.java
@@ -67,14 +67,15 @@ public final class HttpProtocolGeneratorUtils {
         String errorFunctionName = ProtocolGenerator.getOperationErrorDeserFunctionName(
                 operation, context.getProtocolName());
 
-        writer.openBlock("func $L(response $P) error {", "}", errorFunctionName, responseType, () -> {
+        writer.openBlock("func $L(response $P) (metadata interface{}, err error) {", "}",
+                errorFunctionName, responseType, () -> {
             writer.addUseImports(SmithyGoDependency.BYTES);
             writer.addUseImports(SmithyGoDependency.IO);
 
             // Copy the response body into a seekable type
             writer.write("var errorBuffer bytes.Buffer");
             writer.openBlock("if _, err := io.Copy(&errorBuffer, response.Body); err != nil {", "}", () -> {
-                writer.write("return &smithy.DeserializationError{Err: fmt.Errorf("
+                writer.write("return metadata, &smithy.DeserializationError{Err: fmt.Errorf("
                         + "\"failed to copy error response body, %w\", err)}");
             });
             writer.write("errorBody := bytes.NewReader(errorBuffer.Bytes())");
@@ -96,7 +97,7 @@ public final class HttpProtocolGeneratorUtils {
                             error, context.getProtocolName());
                     writer.addUseImports(SmithyGoDependency.STRINGS);
                     writer.openBlock("case strings.EqualFold($S, errorCode):", "", errorId.getName(), () -> {
-                        writer.write("return $L(response, errorBody)", errorDeserFunctionName);
+                        writer.write("return metadata, $L(response, errorBody)", errorDeserFunctionName);
                     });
                 });
 
@@ -107,7 +108,7 @@ public final class HttpProtocolGeneratorUtils {
                         writer.write("Code: errorCode,");
                         writer.write("Message: errorMessage,");
                     });
-                    writer.write("return genericError");
+                    writer.write("return metadata, genericError");
                 });
             });
         });
