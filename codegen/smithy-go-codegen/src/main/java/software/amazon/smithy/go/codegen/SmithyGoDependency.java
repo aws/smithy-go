777a15a7173048116d854a83e55a7ee8c6518ce7
Jason Del Ponte <delpontej@gmail.com>
2020-10-09 15:25:51 -0700

    update gohash and limit travis branch builds

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
@@ -109,6 +109,6 @@ public final class SmithyGoDependency {
     private static final class Versions {
         private static final String GO_STDLIB = "1.14";
         private static final String GO_CMP = "v0.4.1";
-        private static final String SMITHY_GO = "v0.1.1";
+        private static final String SMITHY_GO = "v0.0.0-20201009221937-21015eb9ec4b";
     }
 }
