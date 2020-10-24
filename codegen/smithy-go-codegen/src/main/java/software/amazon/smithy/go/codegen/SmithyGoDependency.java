fc8299b636873e964d45c36c10ee7dd3f60004bb
Sean McGrail <mcgrails@amazon.com>
2020-10-23 18:53:52 -0700

    Update smithy-go version

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
@@ -110,6 +110,6 @@ public final class SmithyGoDependency {
     private static final class Versions {
         private static final String GO_STDLIB = "1.14";
         private static final String GO_CMP = "v0.4.1";
-        private static final String SMITHY_GO = "v0.2.0";
+        private static final String SMITHY_GO = "v0.2.1-0.20201023220843-5834338b6151";
     }
 }
