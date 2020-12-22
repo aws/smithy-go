d3594037306d0f327ee76cd0f30decc17bbfddcc
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2020-12-22 23:29:19 +0100

    Skip generating enum consts for enums without names (#217)

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/EnumGenerator.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/EnumGenerator.java
@@ -49,30 +49,35 @@ final class EnumGenerator implements Runnable {
 
         writer.write("type $L string", symbol.getName()).write("");
 
-        writer.writeDocs(String.format("Enum values for %s", symbol.getName()));
-        Set<String> constants = new LinkedHashSet<>();
-        writer.openBlock("const (", ")", () -> {
-            for (EnumDefinition definition : enumTrait.getValues()) {
-                StringBuilder labelBuilder = new StringBuilder(symbol.getName());
-                String name = definition.getName().orElse(definition.getValue());
-                for (String part : name.split("(?U)[\\W_]")) {
-                    labelBuilder.append(StringUtils.capitalize(part.toLowerCase(Locale.US)));
-                }
-                String label = labelBuilder.toString();
+        // Don't generate constants if there are no explicitly modeled names. We only need to
+        // look at one, since Smithy validates that if one has a name then they must all have
+        // a name.
+        if (enumTrait.getValues().get(0).getName().isPresent()) {
+            writer.writeDocs(String.format("Enum values for %s", symbol.getName()));
+            Set<String> constants = new LinkedHashSet<>();
+            writer.openBlock("const (", ")", () -> {
+                for (EnumDefinition definition : enumTrait.getValues()) {
+                    StringBuilder labelBuilder = new StringBuilder(symbol.getName());
+                    String name = definition.getName().get();
+                    for (String part : name.split("(?U)[\\W_]")) {
+                        labelBuilder.append(StringUtils.capitalize(part.toLowerCase(Locale.US)));
+                    }
+                    String label = labelBuilder.toString();
 
-                // If camel-casing would cause a conflict, don't camel-case this enum value.
-                if (constants.contains(label)) {
-                    LOGGER.warning(String.format(
-                            "Multiple enums resolved to the same name, `%s`, using unaltered value for: %s",
-                            label, name));
-                    label = name;
-                }
-                constants.add(label);
+                    // If camel-casing would cause a conflict, don't camel-case this enum value.
+                    if (constants.contains(label)) {
+                        LOGGER.warning(String.format(
+                                "Multiple enums resolved to the same name, `%s`, using unaltered value for: %s",
+                                label, name));
+                        label = name;
+                    }
+                    constants.add(label);
 
-                definition.getDocumentation().ifPresent(writer::writeDocs);
-                writer.write("$L $L = $S", label, symbol.getName(), definition.getValue());
-            }
-        }).write("");
+                    definition.getDocumentation().ifPresent(writer::writeDocs);
+                    writer.write("$L $L = $S", label, symbol.getName(), definition.getValue());
+                }
+            }).write("");
+        }
 
         writer.writeDocs(String.format("Values returns all known values for %s. Note that this can be expanded in the "
                 + "future, and so it is only as up to date as the client.%n%nThe ordering of this slice is not "
