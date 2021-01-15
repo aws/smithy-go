09631dea532e3476c9e01847f7c32a8e2b747aef
Sean McGrail <mcgrails@amazon.com>
2021-01-14 17:16:01 -0800

    Update FormatEpochSeconds and ParseEpochSeconds to match V1 behaviors

--- time/time_test.go
+++ time/time_test.go
@@ -1,6 +1,8 @@
 package time
 
 import (
+	"math"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -42,16 +44,50 @@ func TestHTTPDate(t *testing.T) {
 }
 
 func TestEpochSeconds(t *testing.T) {
-	refTime := time.Date(2018, 1, 9, 20, 51, 21, 123399936, time.UTC)
-
-	epochSeconds := FormatEpochSeconds(refTime)
-	if e, a := 1515531081.1234, epochSeconds; e != a {
-		t.Errorf("expected %v, got %v", e, a)
+	cases := []struct {
+		reference    time.Time
+		expectedUnix float64
+		expectedTime time.Time
+	}{
+		{
+			reference:    time.Date(2018, 1, 9, 20, 51, 21, 123399936, time.UTC),
+			expectedUnix: 1515531081.123,
+			expectedTime: time.Date(2018, 1, 9, 20, 51, 21, 1.23e8, time.UTC),
+		},
+		{
+			reference:    time.Date(2018, 1, 9, 20, 51, 21, 1e8, time.UTC),
+			expectedUnix: 1515531081.1,
+			expectedTime: time.Date(2018, 1, 9, 20, 51, 21, 1e8, time.UTC),
+		},
+		{
+			reference:    time.Date(2018, 1, 9, 20, 51, 21, 123567891, time.UTC),
+			expectedUnix: 1515531081.123,
+			expectedTime: time.Date(2018, 1, 9, 20, 51, 21, 1.23e8, time.UTC),
+		},
+		{
+			reference: time.Unix(0, math.MaxInt64).UTC(),
+			expectedUnix: 9223372036.854,
+			expectedTime: time.Date(2262, 04, 11, 23, 47, 16, 8.54e8, time.UTC),
+		},
 	}
 
-	parseTime := ParseEpochSeconds(epochSeconds)
+	for i, tt := range cases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			epochSeconds := FormatEpochSeconds(tt.reference)
+			if e, a := tt.expectedUnix, epochSeconds; e != a {
+				t.Errorf("expected %v, got %v", e, a)
+			}
 
-	if e, a := refTime, parseTime; !e.Equal(a) {
+			parseTime := ParseEpochSeconds(epochSeconds)
+
+			if e, a := tt.expectedTime, parseTime; !e.Equal(a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+		})
+	}
+
+	// Check an additional edge that higher precision values are truncated to milliseconds
+	if e, a := time.Date(2018, 1, 9, 20, 51, 21, 1.23e8, time.UTC), ParseEpochSeconds(1515531081.12356); !e.Equal(a) {
 		t.Errorf("expected %v, got %v", e, a)
 	}
 }
