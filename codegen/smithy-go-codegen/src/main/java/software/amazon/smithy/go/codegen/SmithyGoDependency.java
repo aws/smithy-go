5257bafd002ffe8bce9be8c1fd228a835a87db4a
Shantanu Kotambkar <52007797+skotambkar@users.noreply.github.com>
2021-04-08 13:42:43 -0700

    update smithy-go version to point to release v1.3.1 (#285)

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
@@ -118,7 +118,7 @@ public final class SmithyGoDependency {
     private static final class Versions {
         private static final String GO_STDLIB = "1.15";
         private static final String GO_CMP = "v0.5.4";
-        private static final String SMITHY_GO = "v1.3.0";
+        private static final String SMITHY_GO = "v1.3.1";
         private static final String GO_JMESPATH = "v0.4.0";
     }
 }
