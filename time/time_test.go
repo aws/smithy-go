df9b87a49be43175f62cc342758bc8ddc03c5066
Jason Del Ponte <961963+jasdel@users.noreply.github.com>
2021-06-15 14:42:23 -0700

    Update time parsing to not be as strict for HTTPDate and DateTime
    Updates the parsing of HTTPDate and DateTime time formats to not be as
    strict, and allow more variants of the time format. Including UTC
    offsets for DateTime.

--- time/time_test.go
+++ time/time_test.go
@@ -8,20 +8,61 @@ import (
 )
 
 func TestDateTime(t *testing.T) {
-	refTime := time.Date(1985, 4, 12, 23, 20, 50, int(520*time.Millisecond), time.UTC)
-
-	dateTime := FormatDateTime(refTime)
-	if e, a := "1985-04-12T23:20:50.52Z", dateTime; e != a {
-		t.Errorf("expected %v, got %v", e, a)
+	cases := map[string]struct {
+		TimeString      string
+		TimeValue       time.Time
+		SymmetricString bool
+	}{
+		"no offset": {
+			TimeString: "1985-04-12T23:20:50.52Z",
+			TimeValue: time.Date(1985, 4, 12, 23, 20, 50, int(520*time.Millisecond),
+				time.UTC),
+			SymmetricString: true,
+		},
+		"with negative offset": {
+			TimeString: "1985-04-12T23:20:50.52-07:00",
+			TimeValue: time.Date(1985, 4, 12, 23, 20, 50, int(520*time.Millisecond),
+				time.FixedZone("PST", -7*60*60)),
+			SymmetricString: false,
+		},
+		"with positive offset": {
+			TimeString: "1985-04-12T23:20:50.52+07:00",
+			TimeValue: time.Date(1985, 4, 12, 23, 20, 50, int(520*time.Millisecond),
+				time.FixedZone("PST", +7*60*60)),
+			SymmetricString: false,
+		},
 	}
 
-	parseTime, err := ParseDateTime(dateTime)
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			formattedTimeValue := FormatDateTime(c.TimeValue)
+			t.Logf("formated: %v -> %v", c.TimeValue, formattedTimeValue)
 
-	if e, a := refTime, parseTime; !e.Equal(a) {
-		t.Errorf("expected %v, got %v", e, a)
+			// Round Trip time value ensure format and parse are compatible.
+			parsedTimeValue, err := ParseDateTime(formattedTimeValue)
+			t.Logf("parsed formated: %v -> (%v, %v)", formattedTimeValue, parsedTimeValue, err)
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			parsedTimeString, err := ParseDateTime(c.TimeString)
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			// Ensure parsing date time from string matches expected time value.
+			if c.SymmetricString {
+				if e, a := c.TimeString, formattedTimeValue; e != a {
+					t.Errorf("expected %v, got %v", e, a)
+				}
+			}
+			if e, a := c.TimeValue, parsedTimeValue; !e.Equal(a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+			if e, a := c.TimeValue, parsedTimeString; !e.Equal(a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+		})
 	}
 }
 
@@ -40,6 +81,77 @@ func TestHTTPDate(t *testing.T) {
 
 	if e, a := refTime, parseTime; !e.Equal(a) {
 		t.Errorf("expected %v, got %v", e, a)
+	}
+}
+
+func TestParseHTTPDate(t *testing.T) {
+	cases := map[string]struct {
+		date    string
+		expect  time.Time
+		wantErr bool
+	}{
+		"with leading zero on day": {
+			date:   "Fri, 05 Feb 2021 19:12:15 GMT",
+			expect: time.Date(2021, 2, 5, 19, 12, 15, 0, time.UTC),
+		},
+		"without leading zero on day": {
+			date:   "Fri, 5 Feb 2021 19:12:15 GMT",
+			expect: time.Date(2021, 2, 5, 19, 12, 15, 0, time.UTC),
+		},
+		"with double digit day": {
+			date:   "Fri, 15 Feb 2021 19:12:15 GMT",
+			expect: time.Date(2021, 2, 15, 19, 12, 15, 0, time.UTC),
+		},
+		"RFC850": {
+			date:   "Friday, 05-Feb-21 19:12:15 UTC",
+			expect: time.Date(2021, 2, 5, 19, 12, 15, 0, time.UTC),
+		},
+		"ANSIC with leading zero on day": {
+			date:   "Fri Feb 05 19:12:15 2021",
+			expect: time.Date(2021, 2, 5, 19, 12, 15, 0, time.UTC),
+		},
+		"ANSIC without leading zero on day": {
+			date:   "Fri Feb 5 19:12:15 2021",
+			expect: time.Date(2021, 2, 5, 19, 12, 15, 0, time.UTC),
+		},
+		"ANSIC with double digit day": {
+			date:   "Fri Feb 15 19:12:15 2021",
+			expect: time.Date(2021, 2, 15, 19, 12, 15, 0, time.UTC),
+		},
+		"invalid time format": {
+			date:    "1985-04-12T23:20:50.52Z",
+			wantErr: true,
+		},
+		"shortened year with double digit day": {
+			date:   "Thu, 11 Feb 21 11:04:03 GMT",
+			expect: time.Date(2021, 2, 11, 11, 04, 03, 0, time.UTC),
+		},
+		"shortened year without leading zero day": {
+			date:   "Thu, 5 Feb 21 11:04:03 GMT",
+			expect: time.Date(2021, 2, 5, 11, 04, 03, 0, time.UTC),
+		},
+		"shortened year with leading zero day": {
+			date:   "Thu, 05 Feb 21 11:04:03 GMT",
+			expect: time.Date(2021, 2, 5, 11, 04, 03, 0, time.UTC),
+		},
+	}
+
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			result, err := ParseHTTPDate(tt.date)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("error = %v, wantErr = %v", err, tt.wantErr)
+			}
+			if err != nil {
+				return
+			}
+			if result.IsZero() {
+				t.Fatalf("expected non-zero timestamp")
+			}
+			if tt.expect != result {
+				t.Fatalf("expected %q, got %q", tt.expect, result)
+			}
+		})
 	}
 }
 
