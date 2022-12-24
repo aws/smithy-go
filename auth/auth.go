d47ed1d1ea207ebcd2b293ccca3bb1cd4d936aa9
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-12-24 02:53:55 +0100

    chore(auth): include additional comments to types

--- auth/auth.go
+++ auth/auth.go
@@ -3,12 +3,23 @@ package auth
 type key string
 
 const (
+	// The current auth configuration that has been set by any auth middleware and
+	// that will prevent from being set more than once.
 	CURRENT_AUTH_CONFIG key = "currentAuthConfig"
 )
 
+// As described in the Smithy documentation:
+// https://github.com/awslabs/smithy/blob/main/smithy-model/src/main/resources/software/amazon/smithy/model/loader/prelude.smithy
 type HttpAuthDefinition struct {
-	In     string
-	Name   string
+	// Defines the location of where the Auth is serialized.
+	In string
+
+	// Defines the name of the HTTP header or query string parameter
+	// that contains the Auth.
+	Name string
+
+	// Defines the security scheme to use on the `Authorization` header value.
+	// This can only be set if the "in" property is set to {@link HttpAuthLocation.HEADER}.
 	Scheme string
 }
 
