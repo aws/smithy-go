8b815fd6206241665253dac27d3e678044b5d661
Luc Talatinian <lucix@amazon.com>
2023-09-22 16:10:57 -0400

    checkstyle

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/GoUniverseTypes.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/GoUniverseTypes.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.go.codegen;
 
 import software.amazon.smithy.codegen.core.Symbol;
@@ -6,35 +21,36 @@ import software.amazon.smithy.codegen.core.Symbol;
  * Collection of Symbol constants for golang universe types.
  * See <a href="https://go.dev/ref/spec#Predeclared_identifiers">predeclared identifiers</a>.
  */
-public class GoUniverseTypes {
-    public static Symbol Any = universe("any");
-    public static Symbol Bool = universe("bool");
-    public static Symbol Byte = universe("byte");
-    public static Symbol Comparable = universe("comparable");
+public final class GoUniverseTypes {
+    public static Symbol tAny = universe("any");
+    public static Symbol tBool = universe("bool");
+    public static Symbol tByte = universe("byte");
+    public static Symbol tComparable = universe("comparable");
 
-    public static Symbol Complex64 = universe("complex64");
-    public static Symbol Complex128 = universe("complex128");
-    public static Symbol Error = universe("error");
-    public static Symbol Float32 = universe("float32");
-    public static Symbol Float64 = universe("float64");
+    public static Symbol tComplex64 = universe("complex64");
+    public static Symbol tComplex128 = universe("complex128");
+    public static Symbol tError = universe("error");
+    public static Symbol tFloat32 = universe("float32");
+    public static Symbol tFloat64 = universe("float64");
 
-    public static Symbol Int = universe("int");
-    public static Symbol Int8 = universe("int8");
-    public static Symbol Int16 = universe("int16");
-    public static Symbol Int32 = universe("int32");
-    public static Symbol Int64 = universe("int64");
-    public static Symbol Rune = universe("rune");
-    public static Symbol String = universe("string");
+    public static Symbol tInt = universe("int");
+    public static Symbol tInt8 = universe("int8");
+    public static Symbol tInt16 = universe("int16");
+    public static Symbol tInt32 = universe("int32");
+    public static Symbol tInt64 = universe("int64");
+    public static Symbol tRune = universe("rune");
+    public static Symbol tString = universe("string");
 
-    public static Symbol Uint = universe("uint");
-    public static Symbol Uint8 = universe("uint8");
-    public static Symbol Uint16 = universe("uint16");
-    public static Symbol Uint32 = universe("uint32");
-    public static Symbol Uint64 = universe("uint64");
-    public static Symbol Uintptr = universe("uintptr");
+    public static Symbol tUint = universe("uint");
+    public static Symbol tUint8 = universe("uint8");
+    public static Symbol tUint16 = universe("uint16");
+    public static Symbol tUint32 = universe("uint32");
+    public static Symbol tUint64 = universe("uint64");
+    public static Symbol tUintptr = universe("uintptr");
+
+    private GoUniverseTypes() {}
 
     private static Symbol universe(String name) {
         return SymbolUtils.createValueSymbolBuilder(name).putProperty(SymbolUtils.GO_UNIVERSE_TYPE, true).build();
     }
 }
-
