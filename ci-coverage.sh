4aa73baabf0b85ca267e1b562fbe2da753a3db79
Luc Talatinian <lucix@amazon.com>
2023-12-12 20:22:36 -0500

    switch to more compatible bc expr

--- ci-coverage.sh
+++ ci-coverage.sh
@@ -4,7 +4,7 @@ THRESHOLD=75 # percentage
 
 go test -coverprofile c.out ./...
 cov=$(go tool cover -func c.out  | grep '^total:' | awk '{ print $3+0 }')
-if [[ $(bc --expression="$cov < $THRESHOLD") ]]; then
+if [[ $(echo "$cov < $THRESHOLD" | bc) ]]; then
     echo "threshold not met - code must be at or above $THRESHOLD% coverage"
     exit 1
 fi
