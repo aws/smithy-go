59ec59a030744aa519c71adecd475cb8719aa2c1
nathanhit <nathanmohit@gmail.com>
2024-07-18 14:15:26 -0400

    Defined new function for generateRequestSerializers broke down code

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/protocol/aws/AwsJson10ProtocolGenerator.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/protocol/aws/AwsJson10ProtocolGenerator.java
@@ -19,9 +19,11 @@ import static software.amazon.smithy.go.codegen.ApplicationProtocol.createDefaul
 
 import software.amazon.smithy.aws.traits.protocols.AwsJson1_0Trait;
 import software.amazon.smithy.go.codegen.ApplicationProtocol;
+import software.amazon.smithy.go.codegen.GoWriter;
 import software.amazon.smithy.go.codegen.SmithyGoDependency;
 //import software.amazon.smithy.go.codegen.SmithyGoTypes;
 import software.amazon.smithy.go.codegen.integration.ProtocolGenerator;
+import software.amazon.smithy.model.shapes.OperationShape;
 import software.amazon.smithy.model.shapes.ShapeId;
 //import software.amazon.smithy.model.shapes.StructureShape;
 import software.amazon.smithy.utils.SmithyInternalApi;
@@ -44,39 +46,7 @@ public class AwsJson10ProtocolGenerator implements ProtocolGenerator {
         var model = context.getModel();
         var ops = model.getOperationShapes();
         for (var op : ops) {
-            var opName = op.toShapeId().getName();
-
-            //Struct Definition
-            var structName = "awsAwsjson10_serializeOp" + opName;
-            writer.write("type " + structName + " struct{\n}\n");
-
-            //ID Function
-//            var opID = opName + "Serializer";
-// an id for the operation's serializer, that includes the specific op name
-            var opID = "OperationSerializer";
-            var idFunction = "func (op *$L) ID() string {\n return \"$L\" \n}\n";
-            writer.write(idFunction, structName, opID);
-
-            //Handle Serialize Function
-            var handleFunction = """
-                    func (op *$L) HandleSerialize (
-                    ctx $T, in $T, next $T,
-                    )(out $T, metadata $T, err error) {
-                        return next.HandleSerialize(ctx, in)
-                    }
-                    \n""";
-            writer.write(handleFunction, structName, SmithyGoDependency.CONTEXT.interfaceSymbol("Context"),
-                    SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeInput"),
-                    SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeHandler"),
-                    SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeOutput"),
-                    SmithyGoDependency.SMITHY_MIDDLEWARE.struct("Metadata"));
-
-            //Code for if ID were to be contained in
-//            writer.write("\"ID\": $L", opID); //body of struct
-//            writer.write("}"); //close body of struct
-
-            //Ending Operation
-            writer.write("\n");
+            requestSerializerCode(op, writer);
         }
     }
 
@@ -103,5 +73,41 @@ public class AwsJson10ProtocolGenerator implements ProtocolGenerator {
     @Override
     public void generateProtocolDocumentUnmarshalerUnmarshalDocument(GenerationContext context) {
         // TODO
+    }
+
+    private void requestSerializerCode(OperationShape op, GoWriter writer) {
+        var opName = op.toShapeId().getName();
+
+        //Struct Definition
+        var structName = "awsAwsjson10_serializeOp" + opName;
+        writer.write("type " + structName + " struct{\n}\n");
+
+        //ID Function
+//            var opID = opName + "Serializer";
+// an id for the operation's serializer, that includes the specific op name
+        var opID = "OperationSerializer";
+        var idFunction = "func (op *$L) ID() string {\n return \"$L\" \n}\n";
+        writer.write(idFunction, structName, opID);
+
+        //Handle Serialize Function
+        var handleFunction = """
+                    func (op *$L) HandleSerialize (
+                    ctx $T, in $T, next $T,
+                    )(out $T, metadata $T, err error) {
+                        return next.HandleSerialize(ctx, in)
+                    }
+                    \n""";
+        writer.write(handleFunction, structName, SmithyGoDependency.CONTEXT.interfaceSymbol("Context"),
+                SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeInput"),
+                SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeHandler"),
+                SmithyGoDependency.SMITHY_MIDDLEWARE.struct("SerializeOutput"),
+                SmithyGoDependency.SMITHY_MIDDLEWARE.struct("Metadata"));
+
+        //Code for if ID were to be contained in
+//            writer.write("\"ID\": $L", opID); //body of struct
+//            writer.write("}"); //close body of struct
+
+        //Ending Operation
+        writer.write("\n");
     }
 }
