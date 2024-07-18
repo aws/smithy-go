5c6a11d29960da44c64154f9fb670ee1579f8030
nathanhit <nathanmohit@gmail.com>
2024-07-18 13:34:15 -0400

    Revert "Revert "add awsjson10 protocol generator""
    This reverts commit c04f4769f23579ca9a6413f7e09c9a67c233cc70.

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/protocol/aws/AwsJson10ProtocolGenerator.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/protocol/aws/AwsJson10ProtocolGenerator.java
@@ -12,6 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 package software.amazon.smithy.go.codegen.protocol.aws;
 
 import static software.amazon.smithy.go.codegen.ApplicationProtocol.createDefaultHttpApplicationProtocol;
@@ -20,6 +21,7 @@ import software.amazon.smithy.aws.traits.protocols.AwsJson1_0Trait;
 import software.amazon.smithy.go.codegen.ApplicationProtocol;
 import software.amazon.smithy.go.codegen.integration.ProtocolGenerator;
 import software.amazon.smithy.model.shapes.ShapeId;
+//import software.amazon.smithy.model.shapes.StructureShape;
 import software.amazon.smithy.utils.SmithyInternalApi;
 
 @SmithyInternalApi
@@ -36,7 +38,39 @@ public class AwsJson10ProtocolGenerator implements ProtocolGenerator {
 
     @Override
     public void generateRequestSerializers(GenerationContext context) {
-        context.getWriter().get().write("// TODO");
+        var writer = context.getWriter().get();
+        var model = context.getModel();
+        var ops = model.getOperationShapes();
+        for (var op : ops) {
+            var opName = op.toShapeId().getName();
+
+            //Struct Definition
+            var structName = "type " + "awsAwsjson10_serializeOp" + opName + " struct {\n}\n";
+            writer.write(structName);
+
+            //ID Function
+//            var opID = opName + "Serializer"; //an id for the operation's serializer, that includes the specific op name
+            var opID = "OperationSerializer";
+            var idFunction = "func (op *$L) ID() string {\n return \"$L\" \n}\n";
+            writer.write(idFunction, opName, opID);
+
+            //Handle Serialize Function
+            var handleFunction = """
+                    func (op *$L) HandleSerialize
+                    (ctx context.Context, in middleware.SerializeInput, next middleware.SerializeHandler)
+                     (out middleware.SerializeOutput, metadata middleware.Metadata, err error) {
+                        return next.HandleSerialize(ctx, in)
+                    }
+                    \n""";
+            writer.write(handleFunction, opName);
+
+            //Code for if ID were to be contained in
+//            writer.write("\"ID\": $L", opID); //body of struct
+//            writer.write("}"); //close body of struct
+
+            //Ending Operation
+            writer.write("\n");
+        }
     }
 
     @Override
