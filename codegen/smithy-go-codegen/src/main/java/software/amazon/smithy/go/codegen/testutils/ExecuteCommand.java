3956e2cd8eeb134de59cf49ee92a4fdfdbbff58b
Luis Madrigal 🐧 <599908+Madrigal@users.noreply.github.com>
2024-09-20 15:58:57 -0400

    Remove unnecessary imports

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/testutils/ExecuteCommand.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/testutils/ExecuteCommand.java
@@ -19,8 +19,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
