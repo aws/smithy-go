e6338ca9b5ee30c6317ea908173a369a9d3eb236
Luis Madrigal <599908+Madrigal@users.noreply.github.com>
2024-10-08 11:57:37 -0400

    Allow all headers to be set to an empty value (#547)

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/integration/HttpBindingProtocolGenerator.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/integration/HttpBindingProtocolGenerator.java
@@ -852,10 +852,8 @@ public abstract class HttpBindingProtocolGenerator implements ProtocolGenerator 
                     });
         }
 
-        boolean allowZeroStrings = location != HttpBinding.Location.HEADER;
-
         GoValueAccessUtils.writeIfNonZeroValueMember(context.getModel(), context.getSymbolProvider(), writer,
-                memberShape, "v", allowZeroStrings, memberShape.isRequired(), (operand) -> {
+                memberShape, "v", true, memberShape.isRequired(), (operand) -> {
                     final String locationName = binding.getLocationName().isEmpty()
                             ? memberShape.getMemberName() : binding.getLocationName();
                     switch (location) {
@@ -980,6 +978,13 @@ public abstract class HttpBindingProtocolGenerator implements ProtocolGenerator 
         }
 
         MemberShape collectionMemberShape = CodegenUtils.expectCollectionShape(targetShape).getMember();
+        // On empty collection header will be set to an empty value
+        writer.openBlock("if len($L) == 0 {", "}", operand, () -> {
+            writeHttpBindingSetter(context, writer, collectionMemberShape, location, operand,
+                    (w, s) -> {
+                        w.writeInline("$L.AddHeader($L).String(\"\")", dest, locationName);
+                    });
+        });
         writer.openBlock("for i := range $L {", "}", operand, () -> {
             // Only set non-empty non-nil header values
             String indexedOperand = operand + "[i]";
