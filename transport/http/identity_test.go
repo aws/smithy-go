a5fc2c6645a821182048205b46ebbe4975d8db9d
Luis Madrigal <599908+Madrigal@users.noreply.github.com>
2025-03-05 16:47:36 -0500

    Update identity test to not rely on comparing pointers to zero-sized objects (#574)

--- transport/http/identity_test.go
+++ transport/http/identity_test.go
@@ -3,16 +3,15 @@ package http
 import (
 	"context"
 	"testing"
-	smithy "github.com/aws/smithy-go"
+
+	"github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/auth"
 )
 
 func TestIdentity(t *testing.T) {
-	var expected auth.Identity = &auth.AnonymousIdentity{}
-
 	resolver := auth.AnonymousIdentityResolver{}
-	actual, _ := resolver.GetIdentity(context.TODO(), smithy.Properties{})
-	if expected != actual {
-		t.Errorf("Anonymous identity resolver does not produce correct identity")
+	identity, _ := resolver.GetIdentity(context.TODO(), smithy.Properties{})
+	if _, ok := identity.(*auth.AnonymousIdentity); !ok {
+		t.Errorf("Anonymous identity resolver does not produce correct identity, expected it to be of type auth.AnonymousIdentity")
 	}
 }
