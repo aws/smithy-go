035d64d0ccabcc4ee91062cec95df87a1677db33
Luc Talatinian <lucix@amazon.com>
2025-11-13 13:19:53 -0500

    experimental linkedlist conversion

--- middleware/step_initialize.go
+++ middleware/step_initialize.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 // InitializeInput wraps the input parameters for the InitializeMiddlewares to
 // consume. InitializeMiddleware may modify the parameter value before
@@ -72,15 +75,14 @@ var _ InitializeMiddleware = (initializeMiddlewareFunc{})
 // InitializeStep provides the ordered grouping of InitializeMiddleware to be
 // invoked on a handler.
 type InitializeStep struct {
-	ids *orderedIDs
+	head *decoratedInitializeHandler
+	tail *decoratedInitializeHandler
 }
 
 // NewInitializeStep returns an InitializeStep ready to have middleware for
 // initialization added to it.
 func NewInitializeStep() *InitializeStep {
-	return &InitializeStep{
-		ids: newOrderedIDs(baseOrderedItems),
-	}
+	return &InitializeStep{}
 }
 
 var _ Middleware = (*InitializeStep)(nil)
@@ -97,77 +99,142 @@ func (s *InitializeStep) ID() string {
 func (s *InitializeStep) HandleMiddleware(ctx context.Context, in interface{}, next Handler) (
 	out interface{}, metadata Metadata, err error,
 ) {
-	order := s.ids.GetOrder()
-
-	var h InitializeHandler = initializeWrapHandler{Next: next}
-	for i := len(order) - 1; i >= 0; i-- {
-		h = decoratedInitializeHandler{
-			Next: h,
-			With: order[i].(InitializeMiddleware),
-		}
-	}
-
 	sIn := InitializeInput{
 		Parameters: in,
 	}
 
-	res, metadata, err := h.HandleInitialize(ctx, sIn)
+	wnext := &initializeWrapHandler{next}
+	if s.head == nil {
+		res, metadata, err := wnext.HandleInitialize(ctx, sIn)
+		return res.Result, metadata, err
+	}
+
+	s.tail.Next = wnext
+	res, metadata, err := s.head.HandleInitialize(ctx, sIn)
 	return res.Result, metadata, err
 }
 
 // Get retrieves the middleware identified by id. If the middleware is not present, returns false.
 func (s *InitializeStep) Get(id string) (InitializeMiddleware, bool) {
-	get, ok := s.ids.Get(id)
-	if !ok {
-		return nil, false
-	}
-	return get.(InitializeMiddleware), ok
+	return nil, false
 }
 
 // Add injects the middleware to the relative position of the middleware group.
-// Returns an error if the middleware already exists.
+//
+// Add used to return an error but now it does not. Thus, its return value can
+// be ignored.
 func (s *InitializeStep) Add(m InitializeMiddleware, pos RelativePosition) error {
-	return s.ids.Add(m, pos)
+	if s.head == nil {
+		s.head = &decoratedInitializeHandler{nil, m}
+		s.tail = s.head
+		return nil
+	}
+
+	if pos == Before {
+		s.head = &decoratedInitializeHandler{s.head, m}
+	} else {
+		tail := &decoratedInitializeHandler{nil, m}
+		s.tail.Next = tail
+		s.tail = tail
+	}
+
+	return nil
 }
 
 // Insert injects the middleware relative to an existing middleware ID.
 // Returns error if the original middleware does not exist, or the middleware
 // being added already exists.
 func (s *InitializeStep) Insert(m InitializeMiddleware, relativeTo string, pos RelativePosition) error {
-	return s.ids.Insert(m, relativeTo, pos)
+	var prev, found *decoratedInitializeHandler
+	for h := any(s.head); h != nil; h = s.head.Next {
+		if f, ok := h.(*decoratedInitializeHandler); ok && f.With.ID() == m.ID() {
+			found = f
+			break
+		}
+		prev = h.(*decoratedInitializeHandler)
+	}
+	if found == nil {
+		return fmt.Errorf("not found: %s", m.ID())
+	}
+
+	if pos == Before {
+		if prev == nil { // at the front
+			s.head = &decoratedInitializeHandler{s.head, m}
+		} else { // somewhere in the middle
+			prev.Next = &decoratedInitializeHandler{found, m}
+		}
+	} else {
+		if found.Next == nil { // at the end
+			tail := &decoratedInitializeHandler{nil, m}
+			s.tail.Next = tail
+			s.tail = tail
+		} else { // somewhere in the middle
+			found.Next = &decoratedInitializeHandler{found.Next, m}
+		}
+	}
+
+	return nil
 }
 
 // Swap removes the middleware by id, replacing it with the new middleware.
 // Returns the middleware removed, or error if the middleware to be removed
 // doesn't exist.
 func (s *InitializeStep) Swap(id string, m InitializeMiddleware) (InitializeMiddleware, error) {
-	removed, err := s.ids.Swap(id, m)
-	if err != nil {
-		return nil, err
+	for h := any(s.head); h != nil; h = s.head.Next {
+		if f, ok := h.(*decoratedInitializeHandler); ok && f.With.ID() == m.ID() {
+			old := f.With.(InitializeMiddleware)
+			f.With = m
+			return old, nil
+		}
 	}
-
-	return removed.(InitializeMiddleware), nil
+	return nil, fmt.Errorf("not found: %s", m.ID())
 }
 
 // Remove removes the middleware by id. Returns error if the middleware
 // doesn't exist.
 func (s *InitializeStep) Remove(id string) (InitializeMiddleware, error) {
-	removed, err := s.ids.Remove(id)
-	if err != nil {
-		return nil, err
+	var prev, found *decoratedInitializeHandler
+	for h := any(s.head); h != nil; h = s.head.Next {
+		if f, ok := h.(*decoratedInitializeHandler); ok && f.With.ID() == id {
+			found = f
+			break
+		}
+		prev = h.(*decoratedInitializeHandler)
+	}
+	if found == nil {
+		return nil, fmt.Errorf("not found: %s", id)
 	}
 
-	return removed.(InitializeMiddleware), nil
+	if s.head == s.tail { // it's the only one
+		s.head = nil
+		s.tail = nil
+	} else if found == s.head { // at the front
+		s.head = s.head.Next.(*decoratedInitializeHandler)
+	} else if found == s.tail { // at the end
+		prev.Next = nil
+		s.tail = prev
+	} else {
+		prev.Next = found.Next // somewhere in the middle
+	}
+
+	return found.With, nil
 }
 
 // List returns a list of the middleware in the step.
 func (s *InitializeStep) List() []string {
-	return s.ids.List()
+	var ids []string
+	for h := any(s.head); h != nil; h = s.head.Next {
+		if f, ok := h.(*decoratedInitializeHandler); ok {
+			ids = append(ids, f.With.ID())
+		}
+	}
+	return ids
 }
 
 // Clear removes all middleware in the step.
 func (s *InitializeStep) Clear() {
-	s.ids.Clear()
+	s.head = nil
+	s.tail = nil
 }
 
 type initializeWrapHandler struct {
