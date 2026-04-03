2fd6df93a5564ad67e906f38be5b75d0095e6399
Luis Madrigal <599908+Madrigal@users.noreply.github.com>
2026-04-03 11:28:55 -0400

    return a single empty element instead of nil when splitting an empty string

--- endpoints/private/rulesfn/split.go
+++ endpoints/private/rulesfn/split.go
@@ -3,11 +3,11 @@ package rulesfn
 import "strings"
 
 // Split splits the input string by the delimiter and returns the resulting
-// parts. If limit is > 0, at most limit substrings are returned. Returns nil
-// if the input is empty.
+// parts. If limit is > 0, at most limit substrings are returned.
+// Returns a slice with a single empty string if the input is empty.
 func Split(input, delimiter string, limit int) []string {
 	if len(input) == 0 {
-		return nil
+		return []string{""}
 	}
 	if limit > 0 {
 		return strings.SplitN(input, delimiter, limit)
