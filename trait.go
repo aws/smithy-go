194a4fd8a0c426f0b61a3b5fd30c62b3935ab5bf
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2026-05-14 19:48:14 -0400

    make traits indexable (#658)

--- trait.go
+++ trait.go
@@ -6,3 +6,16 @@ package smithy
 type Trait interface {
 	TraitID() ShapeID
 }
+
+// IndexableTrait is optionally implemented by Trait values that have a
+// reserved index in Schema's indexed trait slice. All traits defined in the
+// traits package implement this interface.
+//
+// You SHOULD NOT implement this outside of a smithy-go trait unless you know
+// what you are doing. If you implement this and return a value that collides
+// with one of the primary serde-based indexed traits (see index.go) you will
+// probably break something.
+type IndexableTrait interface {
+	Trait
+	TraitIndex() int
+}
