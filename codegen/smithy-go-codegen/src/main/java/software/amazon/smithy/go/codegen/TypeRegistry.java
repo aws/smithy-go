20b4cc78672ea52d5b00727ea26b2a983d46fc5e
Luc Talatinian <lucix@amazon.com>
2026-05-15 13:33:44 -0400

    fix synthetic namespace collide

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/TypeRegistry.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/TypeRegistry.java
@@ -2,9 +2,14 @@ package software.amazon.smithy.go.codegen;
 
 import static software.amazon.smithy.go.codegen.GoWriter.goTemplate;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.stream.Collectors;
+import software.amazon.smithy.model.knowledge.EventStreamIndex;
+import software.amazon.smithy.model.knowledge.TopDownIndex;
 import software.amazon.smithy.model.loader.Prelude;
 import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.traits.ErrorTrait;
 
 public class TypeRegistry implements Writable {
     private final GoCodegenContext ctx;
@@ -15,10 +20,28 @@ public class TypeRegistry implements Writable {
 
     @Override
     public void accept(GoWriter writer) {
-        var shapes = ctx.serdeShapes(StructureShape.class).stream().toList();
+        var model = ctx.model();
+        var service = ctx.service();
+
+        // Errors: needed for DeserializableError lookup
+        var shapes = new HashSet<StructureShape>();
+        shapes.addAll(model.getStructureShapesWithTrait(ErrorTrait.class));
+
+        // Event stream events: needed for LookupEntry by target ID
+        var streamIndex = EventStreamIndex.of(model);
+        for (var op : TopDownIndex.of(model).getContainedOperations(service)) {
+            streamIndex.getInputInfo(op).ifPresent(info ->
+                    info.getEventStreamTarget().members().forEach(m ->
+                            shapes.add(model.expectShape(m.getTarget(), StructureShape.class))));
+            streamIndex.getOutputInfo(op).ifPresent(info ->
+                    info.getEventStreamTarget().members().forEach(m ->
+                            shapes.add(model.expectShape(m.getTarget(), StructureShape.class))));
+        }
+
+        var sorted = shapes.stream().sorted().collect(Collectors.toList());
 
         writer.addUseImports(SmithyGoDependency.SMITHY);
-        if (shapes.stream().anyMatch(Prelude::isPublicPreludeShape)) {
+        if (sorted.stream().anyMatch(Prelude::isPublicPreludeShape)) {
             writer.addUseImports(SmithyGoDependency.SMITHY_PRELUDE);
         }
         writer.addImport(ctx.settings().getModuleName() + "/schemas", "schemas");
@@ -29,7 +52,7 @@ public class TypeRegistry implements Writable {
                         $entries:W
                     },
                 }
-                """, Map.of("entries", Writable.map(shapes, this::renderEntry)));
+                """, Map.of("entries", Writable.map(sorted, this::renderEntry)));
     }
 
     private Writable renderEntry(StructureShape shape) {
